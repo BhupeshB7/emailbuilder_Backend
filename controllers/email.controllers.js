b7bc0151788a5ddd02ed26a14b611e202cd7a968
BhupeshB7 <bhupeshkr2912@gmail.com>
2025-01-20 14:29:52 +0530

    render template

--- controllers/email.controllers.js
+++ controllers/email.controllers.js
@@ -1,12 +1,18 @@
 import fs from "fs/promises";
 import path from "path";
+import { fileURLToPath } from "url";
 import Handlebars from "handlebars";
+ 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename) 
+const TEMPLATE_PATH = path.join(__dirname, "templates", "layout.html");
 
 export const getEmailLayout = async (req, res) => {
     try {
-        const layout = await fs.readFile("./templates/layout.html", "utf-8");
+        const layout = await fs.readFile(TEMPLATE_PATH, "utf-8");
         res.send(layout);
     } catch (error) {
+        console.error("Error reading template file:", error);
         res.status(500).send("Error reading template file");
     }
 };
@@ -14,7 +20,7 @@ export const getEmailLayout = async (req, res) => {
 export const renderAndDownloadTemplate = async (req, res) => {
     try {
         const config = req.body;
-        const layout = await fs.readFile("templates/layout.html", "utf-8");
+        const layout = await fs.readFile(TEMPLATE_PATH, "utf-8");
 
         const template = Handlebars.compile(layout);
         const rendered = template(config);
@@ -23,7 +29,7 @@ export const renderAndDownloadTemplate = async (req, res) => {
         res.setHeader("Content-Disposition", "attachment; filename=email-template.html");
         res.send(rendered);
     } catch (error) {
-        console.log(error);
+        console.error("Error rendering template:", error);
         res.status(500).send("Error rendering template");
     }
 };
